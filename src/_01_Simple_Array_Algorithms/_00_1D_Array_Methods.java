620f6bff9fbbe662f31b6f3518e2a75ed1f20f4b
sofiamv <league@WTS20.attlocal.net>
2018-12-08 15:54:03 -0800

    Visual Array Sorter, 1D Array Methods, Firework(IP)
    dkfklklrseodhwgovn yreuihvgn y8erif759vje8uc vm,58ugti vm,rufjvm
    wsrdfgvhbjmkl,;.sdfcgbjhnoikjlmsdvo;lkqawsdcvoplk,mwsfxcouj

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,31 +4,47 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int gah = 0;
+		for(int i = 0; i < values.length; i++) {
+			gah = gah + values[i];
+		}
+		return gah;
 	}
-	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		int gah = 0;
+		for(int i = 0; i < values.length; i++) {
+			gah = (gah + values[i])/ i++;
+		}
+		return gah;
 	}
-	
-	
 	//3. Complete the method so that it returns true if the integer
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
+	boolean bad = false;
+	for(int i = 0; i < array.length; i++) {
+		if(array[i] == value) {
+			bad = true;
+		}else {
+			bad = false;
+		}
 	}
-	
+		return bad;
+	}
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-		
-		return 0;
+		int asdfghjkl = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == value) {
+				asdfghjkl = i;
+			}else {
+				asdfghjkl = -1;
+			}
+		}
+		return asdfghjkl;
 	}
 }
--- src/_02_Firework_Display/Firework.java
+++ src/_02_Firework_Display/Firework.java
@@ -1,31 +1,26 @@
 package _02_Firework_Display;
-
 import java.awt.Graphics;
 import java.util.Random;
-
 /*
  * 
  * You can think of a fire work as an array of sparks. Run the demo jar to see the finished product
  * then complete the steps. 
  *
  */
-
 public class Firework{
 	public static final int GRAVITY = 1;
-	
 	//1. Create an array of Spark objects called sparks. Don't initialize it.
-	
+	Spark [] sparks;
 	public boolean dead = false;
-	
 	public Firework() {
 		//2. Initialize the Spark array to contain 100 Sparks. 
-
-		
+		sparks = new Spark[100];
 		//3. Iterate through the sparks and initialize each one to a new Spark.
 		//   Make each spark start at the middle bottom of the screen.
-	
+	for(int i = 0; i<sparks.length; i++) {
+		sparks[i] = new Spark(300, 300);
 	}
-	
+	}
 	public void launch() {
 		//4. Iterate through the sparks and reset their x and y location
 		//   to their original starting point.
